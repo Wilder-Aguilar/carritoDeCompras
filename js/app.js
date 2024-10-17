be163fd342df55e85e812e2123fcbbc72e16b8f4
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-10-17 23:47:36 +0200

    seleccion de curso finalizado

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+//lee html y extrae info
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+}
+
+//objeto con contendo del curso
+const infoCurso ={
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAtributte('data-id'),
+    cantidad : 1
+}
